a9d441d25e99bafeab8178792b2e5fdcafd5dabe
Jamie Rushford <jar.rushford@gmail.com>
2019-02-25 13:43:13 -0700

    Finish home page, except actual link paths

--- src/components/AdminHome/AdminHome.js
+++ src/components/AdminHome/AdminHome.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import '../../main.scss'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-export default function AdminHome() {
-  return (
-    <div className="admin-home">
-      <header>
-        <NavLink to="/" className="nav-item">Tour</NavLink>
-        <NavLink to="/" className="nav-item">Videos</NavLink>
-        <NavLink to="/" className="nav-item">News</NavLink>
-        <NavLink to="/" className="nav-item">Photos</NavLink>
-        <NavLink to="/" className="nav-item">Mailing</NavLink>
-      </header>
-    </div>
-  )
+export function AdminHome({ user }) {
+  if (!user) {
+    return <Redirect to="/admin" />
+  } else {
+    return (
+      <div className="admin-home">
+        <header>
+          <NavLink to="/" className="nav-item">Tour</NavLink>
+          <NavLink to="/" className="nav-item">Videos</NavLink>
+          <NavLink to="/" className="nav-item">News</NavLink>
+          <NavLink to="/" className="nav-item">Photos</NavLink>
+          <NavLink to="/" className="nav-item">Mailing</NavLink>
+        </header>
+        <h1 className="title">Welcome to the Mad Wallace Admin Portal</h1>
+      </div>
+    )
+  }
 }
+
+export const mapStateToProps = (state) => ({
+  user: state.user
+})
+
+export default connect(mapStateToProps)(AdminHome)
