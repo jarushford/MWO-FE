fbe6b4d078f8fc34bbeceb2ce30e798a23c1ef8e
Jamie Rushford <jar.rushford@gmail.com>
2019-02-25 11:08:04 -0700

    Finish functionality of login component

--- src/components/AdminLogin/AdminLogin.js
+++ src/components/AdminLogin/AdminLogin.js
@@ -1,15 +1,45 @@
 import React, { useState } from 'react'
+import { sha256 } from 'js-sha256'
+import { Redirect } from 'react-router-dom'
 
-export function AdminLogin() {
+export default function AdminLogin() {
   const [email, updateEmail] = useState('')
   const [password, updatePassword] = useState('')
+  const [isLoggedIn, setLogin] = useState(false)
+  const [hasErrored, setError] = useState(false)
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const hashedPassword = sha256(password)
+    const user = { email, password: hashedPassword }
+    const url = 'https://mwo-be.herokuapp.com/api/v1/login'
+    
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user)
+    })
+
+    !response.ok
+      ? setError(true)
+      : setLogin(true)
+  }
+
+  if (isLoggedIn) {
+    return <Redirect to="/" />
+  }
 
   return (
     <div className="admin-login">
       <h1>Login</h1>
+      <p className={`err-msg ${hasErrored && 'error'}`}>
+        Incorrect username or password
+      </p>
       <form
         className="login-form"
-        onSubmit={e => handleSubmit(e, email, password)}
+        onSubmit={e => {
+          return handleSubmit(e)
+        }}
       >
         <input
           type="text"
@@ -18,7 +48,7 @@ export function AdminLogin() {
           onChange={e => updateEmail(e.target.value)}
         />
         <input
-          type="text"
+          type="password"
           placeholder="password"
           value={password}
           onChange={e => updatePassword(e.target.value)}
@@ -27,9 +57,4 @@ export function AdminLogin() {
       </form>
     </div>
   )
-}
-
-export function handleSubmit(e, email, password) {
-  e.preventDefault()
-
 }
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import { AdminLogin } from '../AdminLogin/AdminLogin'
+import AdminLogin from '../AdminLogin/AdminLogin'
 
 export default function App() {
   return (
