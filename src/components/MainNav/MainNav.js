21c4033161700437effe3702dc0bd1fdfe0289a7
Jamie Rushford <jar.rushford@gmail.com>
2019-04-17 21:14:37 -0600

    Finish header styling

--- src/components/MainNav/MainNav.js
+++ src/components/MainNav/MainNav.js
@@ -4,16 +4,19 @@ import '../../main.scss'
 
 export default function MainNav() {
   return (
-    <header className="main-nav">
-      <NavLink to="/tour">tour</NavLink>
-      <NavLink to="/media">media</NavLink>
-      <Link to="/">
-        <div className="header-logo">
-          <img src="./assets/main-logo.png" />
-        </div>
-      </Link>  
-      <NavLink to="/about">about</NavLink>
-      <NavLink to="/contact">contact</NavLink>
-    </header>
+    <section>
+      <header className="main-nav">
+        <NavLink to="/tour">tour</NavLink>
+        <NavLink to="/media">media</NavLink>
+        <Link to="/">
+          <div className="header-logo">
+            <img alt="mw-logo" src="./assets/main-logo.png" />
+          </div>
+        </Link>  
+        <NavLink to="/about">about</NavLink>
+        <NavLink to="/contact">contact</NavLink>
+      </header>
+      <div className="banner-img" />
+    </section>
   )
 }
