a17816c3b152b54fe1265fe47fe0e7432525d65d
Jamie Rushford <jar.rushford@gmail.com>
2019-04-20 17:45:04 -0600

    Add music and photos carousel to media

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -12,6 +12,7 @@ class Media extends Component {
     this.state = {
       videos: [],
       photos: [],
+      currentImage: 0,
       isLoading: false,
       error: ''
     }
@@ -64,29 +65,86 @@ class Media extends Component {
     }
   }
 
+  nextPhoto = (dir) => {
+    const { currentImage, photos } = this.state
+    if (dir === 1 && currentImage === photos.length - 1) {
+      this.setState({ currentImage: 0 })
+    } else if (dir === -1 && currentImage === 0) {
+      this.setState({ currentImage: photos.length - 1 })
+    } else {
+      this.setState({ currentImage: currentImage + dir })
+    }
+  }
+
   render() {
-    if (this.state.isLoading) {
+    const { photos, videos, currentImage, isLoading, error } = this.state
+
+    if (isLoading) {
       return <Loader />
     }
 
-    if (this.state.error.length) {
-      return <ErrorPage message={this.state.error} />
+    if (error.length) {
+      return <ErrorPage message={error} />
+    }
+
+    // let videoRender
+    // if (!this.state.videos.length) {
+    //   videoRender = <h3>videos are loading</h3>
+    // } else {
+    //   videoRender = (
+    //     <div>
+    //         {
+    //           this.state.videos.map(video => {
+    //             return (
+    //               <div className="vid-container">
+    //                 <iframe id="vid_frame" src={video.link} frameBorder="0" width="100%" height="100%"></iframe>
+    //               </div>
+    //             )
+    //           })
+    //         }
+    //     </div>
+    //   )
+    // }
+
+    let photosRender
+    if (!photos.length) {
+      photosRender = <h3>photos are loading</h3>
+    } else {
+      photosRender = (
+        <img
+          src={photos[currentImage].link}
+          alt={photos[currentImage].description}
+        />
+      )
     }
 
     return (
       <section className="media-container">
         <h1>media</h1>
         <section className="music-container">
-
+          <h2>music</h2>
+          <div>
+            <iframe id="spotifyPlayer" src="https://open.spotify.com/embed/user/1243259682/playlist/1dvSwaN3D8PGP8zWNGz2UC" title="spotify player" width="300px" height="300px" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            <a href="https://madwallace.bandcamp.com/album/hook-line-sinker" target="_blank" rel="noopener noreferrer" >
+              <img id="hookline" src="./assets/hookline-album-art.jpg" alt="Hook Line and Sinker" />
+            </a>
+			      <a href="https://madwallace.bandcamp.com/album/2804" target="_blank" rel="noopener noreferrer">
+              <img id="twenty804" src="./assets/2804-album-art.jpg" alt="2804" />
+            </a>
+          </div>
+          <button>follow on spotify <i className="fab fa-spotify"></i></button>
         </section>
         <section className="videos-container">
-
+          <h2>videos</h2>
+          {/* {videoRender} */}
         </section>
         <section className="photos-container">
-
-        </section>
-        <section className="instagram-container">
-
+          <h2>photos</h2>
+          <div>
+            <i className="fas fa-caret-left" onClick={() => this.nextPhoto(-1)}></i>
+            {photosRender}
+            <i className="fas fa-caret-right" onClick={() => this.nextPhoto(1)}></i>
+          </div>
         </section>
       </section>
     )
