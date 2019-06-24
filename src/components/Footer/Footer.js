6c13759da88778852de4de10264d0093fd37358b
Jamie Rushford <jamierushford@CECELIA-UI-MAC01s-MacBook-Pro.local>
2019-06-24 12:20:26 -0600

    Add link attrs in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -58,22 +58,22 @@ export default class Footer extends Component {
             <span className="text-green">Subscribe</span>
           </button>
           <div className="social-container">
-            <a href="https://www.facebook.com/madwallaceband/" target="_blank" ref="noopener noreferrer">
+            <a href="https://www.facebook.com/madwallaceband/" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-facebook-square" />
             </a>
-            <a href="https://www.instagram.com/mad.wallace/" target="_blank" ref="noopener noreferrer">
+            <a href="https://www.instagram.com/mad.wallace/" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-instagram" />
             </a>
-            <a href="https://open.spotify.com/artist/6H2CmXWubR7YVd6n1hlXq7" target="_blank" ref="noopener noreferrer">
+            <a href="https://open.spotify.com/artist/6H2CmXWubR7YVd6n1hlXq7" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-spotify" />
             </a>
             <a href="mailto:madwallaceband@gmail.com">
               <i className="fas fa-envelope" />
             </a>
-            <a href="https://www.youtube.com/channel/UC6N29y6h0Fqh7VmEObN6LHA" target="_blank" ref="noopener noreferrer">
+            <a href="https://www.youtube.com/channel/UC6N29y6h0Fqh7VmEObN6LHA" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-youtube" />
             </a>
-            <a href="https://www.youtube.com/watch?v=_lvB2tHSEG4" target="_blank" ref="noopener noreferrer">
+            <a href="https://www.youtube.com/watch?v=_lvB2tHSEG4" target="_blank" rel="noopener noreferrer">
               <i className="fas fa-bath" />
             </a>
           </div>
